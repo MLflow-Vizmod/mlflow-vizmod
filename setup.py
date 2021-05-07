8ff95f93a615fc7e7c0187703fe7709301a85610
Hibbard, James <James.Hibbard@seattlechildrens.org>
2021-05-07 15:22:25 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_namespace_packages
+
+
+APP_NAME = "mlflow_vizmod"
+VERSION = "1.1.1"
+LICENSE = "MIT"
+AUTHOR = "James Hibbard, Andrew Bauman"
+DESCRIPTION = "MLflow model flavor for managing visualizations as models"
+
+setup(
+    name=APP_NAME,
+    version=VERSION,
+    license=LICENSE,
+    author=AUTHOR,
+    description=DESCRIPTION,
+    install_requires=[
+        "mlflow>=1.11.0",
+        "PyYAML>=5.3.1",
+        "altair>=4.1.0",
+        "altair_viewer>=0.3.0",
+    ],
+    extras_require={},
+    packages=find_namespace_packages("src"),
+    package_dir={"": "src"},
+    package_data={
+        "": ["*.yaml"],
+    },
+    entry_points="""
+    [console_scripts]
+    """,
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
---
+++ tests/test_logging.py
@@ -0,0 +1,63 @@
+# Standard Libraries
+import os
+
+# External Libraries
+import mlflow
+import altair as alt
+import pandas as pd
+
+# Internal Libraries
+import mlflow_vizmod
+
+
+def get_latest_run():
+    client = mlflow.tracking.MlflowClient()
+    return client.get_run(client.list_run_infos(experiment_id='0')[0].run_id)
+
+
+def test_logging():
+    df = pd.DataFrame({
+        'x': [0, 1, 2, 3, 4, ],
+        'y': [0, 1, 2, 3, 4, ]
+    })
+
+    viz = alt.Chart(
+        df
+    ).mark_circle(size=60).encode(
+        x='x',
+        y='y',
+    )
+
+    mlflow_vizmod.log_model(
+        model=viz,
+        artifact_path='viz',
+        style='altair',
+    )
+
+
+def test_loading():
+    run = get_latest_run()
+    viz = mlflow_vizmod.load_model(
+        model_uri=os.path.join(
+            run.info.artifact_uri,
+            'viz',
+        ),
+        style='altair',
+    )
+
+
+def test_display():
+    df = pd.DataFrame({
+        'x': [1, 2, 3, 4, 5, ],
+        'y': [1, 2, 3, 4, 5, ]
+    })
+
+    run = get_latest_run()
+    viz = mlflow_vizmod.load_model(
+        model_uri=os.path.join(
+            run.info.artifact_uri,
+            'viz',
+        ),
+        style='altair',
+    )
+    viz.display(df)
